5365acfae7f5fbaf17cce296deda7844a4581719
Anna McKelvey <anna.e.mckelvey@gmail.com>
2014-02-28 14:59:38 -0600

    Added isValidBarCode and other helper methods

--- BarCode.java
+++ BarCode.java
@@ -11,29 +11,245 @@
  *  @version: Last Modified ________, 2014
  */
 
+import java.util.*;
+
 public class BarCode {
 
 	// data fields
 	private String myZipCode; // five-digit zip code
 	private String myBarCode; // represents 32-character bar code
 
-	// constructor zip
-
+	// constructor
 	public BarCode (String barOrZip) {
 
-		// check if zip code
+		// check if zip code length
 		if (barOrZip.length() == 5) {
-			myZipCode = barOrZip;
-			// initialize myBarCode
-			myBarCode = encode(myZipCode); 
+			// if string is a valid zip (numeric)
+			if (isNumeric(barOrZip))
+			{
+				myZipCode = barOrZip;
+				// initialize myBarCode
+				myBarCode = encode(myZipCode); 
+			}
+			// else set variables to null to trigger error message
+			else 
+				{
+					myBarCode = null;	
+					myZipCode = null;
+				}
 		}
-		// check if bar code
+
+		// check if bar code length
 		else if (barOrZip.length() == 32) {
-			myBarCode = barOrZip;
-			// initialize myZipCode
-			myZipCode = decode(myBarCode); 
+			// check if valid bar code
+			if(isValidBarCode(barOrZip))
+			{
+				myBarCode = barOrZip;
+				// initialize myZipCode
+				myZipCode = decode(myBarCode); 
+			}
+			// else set variables to null to trigger error message
+			else 
+			{
+					myBarCode = null;	
+					myZipCode = null;
+			}	
 		}
-			else throw new IllegalArgumentException();	
+			else
+			{
+				myBarCode = null;
+				myZipCode = null;
+			}
 	}
 
+	// getter methods
+	public String getBarCode()
+	{
+		return myBarCode;
+	}
+
+	public String getZipCode()
+	{
+		return myZipCode;
+	}
+
+	private boolean isValidBarCode(String barCode)
+	{
+		// build a string to be checked
+		StringBuilder digits = new StringBuilder();
+
+		// initialize array of bar segments
+		String [] barSegments = new String [6]; // maybe use a constant?
+
+		// counter for bar segments
+		int j = 1;
+
+		// populate array
+		for (int  i = 0; i < 6; i++)
+		{
+			barSegments[i] = barCode.substring(j, j + 5);
+			j = j+5;
+		}
+
+		// decode bar segments and append to string to be checked
+		for (int i = 0; i < 6; i++)
+		{
+			digits.append(codeToDigit(barSegments[i]));
+		}
+
+		String s = digits.toString();
+
+		// stores the sum of the zip code digits
+		int digitSum = 0;
+
+		// check that all entries are digits and that check digit is correct
+		if (isNumeric(s))
+			{
+				for (int 1 = 0; i < s.length() - 1; i++)
+				{
+					digitSum += s.charAt(i);
+				}
+
+				// check if the last digit in the string equals check sum
+				if (s.charAt(5) == getCheckDigit(digitSum))
+				{
+					return true;
+				}
+				else return false;
+			}
+
+	}
+
+	// takes a zip code and returns the bar code
+	private String encode (String zipCode)
+	{
+		// this is the check digit in a bar code
+		int checkDigit = 0;
+		// sum of zip code digits to calculate check digit
+		int digitSum = 0;
+		// start building bar code
+		StringBuilder barCode = new StringBuilder();
+		// first frame bar
+		barCode.append("|");
+		// append 5 bars per zip digit
+		for (int i = 0; i < zipCode.length(); i++)
+		{
+			// lookup bar sequence for a digit
+			barCode.append(digitToCode(zipCode.charAt(i)));
+			// add digit to check sum
+			digitSum += zipCode.charAt(i);
+		}
+		// calculate check digit
+		checkDigit = getCheckDigit(digitSum);
+		
+		// append check digit bar sequence to bar code
+		barCode.append(digitToCode(Character.forDigit(checkDigit, 10)));
+		// last frame bar
+		barCode.append("|");
+
+		// return the bar code as a string
+		return barCode.toString();
+	}
+
+	// takes a bar code and returns the zip code
+	private String decode (String barCode)
+	{
+		// start building zip code
+		StringBuilder zipCode = new StringBuilder();
+
+		// initialize array of bar segments
+		String [] barSegments = new String [5]; // maybe use a constant?
+
+		// counter for bar segments
+		int j = 1;
+
+		// populate array
+		for (int  i = 0; i < 5; i++)
+		{
+			barSegments[i] = barCode.substring(j, j + 5);
+			j = j+5;
+		}
+
+		// decode bar segments and append to zip code string builder
+		for (int i = 0; i < 5; i++)
+		{
+			zipCode.append(codeToDigit(barSegments[i]));
+		}
+
+		return zipCode.toString();
+	}
+
+	// returns s the integer 0-9  that is necessary for the sum of the 
+	// digits to equal the next multiple of 10
+	private int getCheckDigit(int sum)
+	{
+		if (sum % 10 != 0) 		// if digitSum is not already a multiple of 10
+		{
+			return 10 - (sum % 10);
+		}
+		else return 0;
+	}
+
+	// helper function to lookup bar sequence for each digit
+	private String digitToCode(char c) 
+	{
+		String barSequence = "";
+		switch (c) {
+			case '0': barSequence = "||:::";
+					break;
+			case '1': barSequence = ":::||";
+					break;
+			case '2': barSequence = "::|:|";
+					break;
+			case '3': barSequence = "::||:";
+					break;
+			case '4': barSequence = ":|::|";
+					break;
+			case '5': barSequence = ":|:|:";
+					break;
+			case '6': barSequence = ":||::";
+					break;
+			case '7': barSequence = "|:::|";
+					break;
+			case '8': barSequence = "|::|:";
+					break;
+			case '9': barSequence = "|:|::";
+					break;
+		}
+		return barSequence;
+	}
+
+	// helper function to lookup digit for each bar sequence
+	private char codeToDigit(String s) 
+	{
+		char zipDigit = '\0';
+		switch (s) {
+			case "||:::": zipDigit = '0';
+					break;
+			case ":::||": zipDigit = '1';
+					break;
+			case "::|:|": zipDigit = '2';
+					break;
+			case "::||:": zipDigit = '3';
+					break;
+			case ":|::|": zipDigit = '4';
+					break;
+			case ":|:|:": zipDigit = '5';
+					break;
+			case ":||::": zipDigit = '6';
+					break;
+			case "|:::|": zipDigit = '7';
+					break;
+			case "|::|:": zipDigit = '8';
+					break;
+			case "|:|::": zipDigit = '9';
+					break;	
+		}
+		return zipDigit;
+	}
+
+	// helper function to figure out if the zip code is valid
+	private boolean isNumeric(String s) {  
+        return s.matches("\\d+");  
+    } 
 }
--- BarCodeTest.java
+++ BarCodeTest.java
@@ -4,18 +4,23 @@ public class BarCodeTest
 {
 	public static void main (String [] args) 
 	{
-		// test 1 to make a bar code out of zip code
+	/*	// test 1 to make a bar code out of zip code
 		BarCode bc1 = new BarCode ("02138");
-		// //remember to check that zip is valid in the constructor
-		System.out.println ("Converting zip code 02138 into bar code... ");
-		System.out.println ("The bar code for zip 02138 is " + bc1.getBarCode() + "\n");
-
-		// test 1 to make a zip code out of a bar code
+		if (bc1.getBarCode() == null) 
+		{
+			System.out.println("Error");
+		}
+		else 
+		{
+			System.out.println ("\nConverting zip code 02138 into bar code... ");
+			System.out.println ("The bar code for zip 02138 is " + bc1.getBarCode() + "\n");
+		}
+	// test 1 to make a zip code out of a bar code
 		BarCode zc1 = new BarCode ("||:|:::|:|:||::::::||:|::|:::|||");
 		// //remember to check that zip is valid in the constructor
 		System.out.println ("Converting bar code into zip code...");
 		System.out.println ("The zip code for bar code ||:|:::|:|:||::::::||:|::|:::||| is " + zc1.getZipCode() + "\n");
-
+	*/
 		// test 2 to make a bar code out of zip code
 		BarCode bc2 = new BarCode ("63124");
 		// //remember to check that zip is valid in the constructor
@@ -26,7 +31,7 @@ public class BarCodeTest
 		// //remember to check that zip is valid in the constructor
 		System.out.println ("Converting bar code into zip code...");
 		System.out.println ("The zip code for bar code " + zc2.getBarCode() + "is " + zc2.getZipCode() + "\n");
-
+/*
 		// test 3 to make a zip code out of a bar code
 		BarCode zc3 = new BarCode ("||:::||::|:::|:|:|::|||:::||:::|");
 		// // remember to check that zip is valid in the constructor
@@ -45,5 +50,8 @@ public class BarCodeTest
 		System.out.println ("Converting bar code into zip code...");
 		System.out.println ("The zip code for bar code ||:|:::|:|:||::::::||:|::|:::||: is " + zc5.getZipCode() + "\n");
 
+
+
+*/
 	}
 }
